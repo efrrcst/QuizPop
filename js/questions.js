68c7952bf379c9d44ccd1b0d6031f7d6d95d41f3
efrrcst <efrrcst@gmail.com>
2022-06-06 18:23:47 -0300

    subindo o quiz

---
+++ js/questions.js
@@ -0,0 +1,124 @@
+let questions = [
+    {
+      numb: 1,
+      image: 'img/sullivan.jpg', //-> quando o parametro for criado, habilitar
+      question: "Um orfão descobre que herdou um presente dos seus pais,com isso ele usa o presente para ajudar outras pessoas.:",
+      answer: "Harry Potter",
+      options: [
+        "Batman",
+        "Harry potter",
+        "Boruto",
+        "A invenção de Hugo Cabret"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Um menino desaparece, viaja no tempo e no final quem acaba viajando é você.:",
+    answer: "Dark",
+    options: [
+      "Dark",
+      "De volta para o futuro",
+      "Ricky e Morty",
+      "Contratempo"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Homem que chuta pobre mas se apaixona por uma menina pobre que fere o ego dele.:",
+    answer: "Orgulho e Preconceito",
+    options: [
+      "Jogo vorazes",
+      "Orgulho e preconceito",
+      "Uma linda mulher",
+      "Razão e Sensibilidade"
+    ]
+  }, 
+    {
+    numb: 4,
+    question: "mulher vaza e ele vira multimilionário.:",
+    answer: "A procura da felicidade",
+    options: [
+      "Prenda-me se for capaz",
+      "O lobo de Wall Street",
+      "A procura da felicidade",
+      "Proposta indecente"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Jovem mulher não reconhece doença mental e casa com seu sequestrador.:",
+    answer: "Bela e a Fera",
+    options: [
+      "365 Dias",
+      "Bela e a Fera",
+      "Refém da paixão",
+      "Esquadrão Suicida "
+    ]
+  },
+    {
+    numb: 6,
+    question: "Uma menina viveu e cárcere privado até que um ladrão invade a sua casa e a leva para cidade em troca de um recompensa.:",
+    answer: "Enrolados",
+    options: [
+      "Enrolados",
+      "Sherk 1",
+      "Um Contratempo",
+      "pocahontas 2"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Onde você conhece uma cidade do interior que tem mais crimes do que o Rio de janeiro?:",
+    answer: "Riverdalle",
+    options: [
+      "Gothan",
+      "Riverdalle",
+      "Gilmore Girls",
+      "Joker"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Ele vai visitar os pais da companheira pela primeira vez,e descobre que eles são preconceituosos e bolaram um plano para outro cara roubar sua identidade.:",
+    answer: "Sherk 2",
+    options: [
+      "Sherk 2 ",
+      "Corra",
+      "Cuckoo",
+      "Historias Cruzadas"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Dois homens se disfarçam de mulher,convivem com meninas ricas para desvendar um crime.:",
+    answer: "A vovózona",
+    options: [
+      "As branquelas",
+      "Bad Boys",
+      "A vovózona",
+      "Tootsie"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Pensaram que ele tinha morrido, mas a lei do amor trouxe ele de volta de um mundo paralelo.:",
+    answer: "Interestellar",
+    options: [
+      "Stranger Things",
+      "Interestellar",
+      "Star Wars",
+      "A jornada"
+    ]
+  },
+    {
+    numb: 11,
+    question: "De qual filme é essa cena?",
+    answer: "Monstros S.A",
+    options: [
+      "Encanto",
+      "Monstros S.A",
+      "Universidade Monstros ",
+      "pocahontas"
+    ]
+  },
+];
